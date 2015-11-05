c7bc6f7c466e54e228a2200cca6849736b314156
Nite Luo <darkdukey@gmail.com>
2015-11-04 17:25:22 -0800

    Update fyber sdk for js

--- js/frameworks/runtime-src/proj.android/jni/Application.mk
+++ js/frameworks/runtime-src/proj.android/jni/Application.mk
@@ -7,12 +7,12 @@ APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-ch
 APP_LDFLAGS := -latomic
 
 ifeq ($(NDK_DEBUG),1)
-  APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
-  APP_OPTIM := debug
+APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
+APP_OPTIM := debug
 else
-  APP_CPPFLAGS += -DNDEBUG
-  APP_OPTIM := release
+APP_CPPFLAGS += -DNDEBUG
+APP_OPTIM := release
 endif
 
 
-APP_PLATFORM := android-14
+APP_PLATFORM := android-14 android-9
